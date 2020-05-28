7f05af2993e47349e5548e4b73c00204bf50e722
Leo Carolan <lca84@uclive.ac.nz>
2020-05-28 20:44:45 +1200

    Update acceleration.h

--- acceleration.h
+++ acceleration.h
@@ -16,18 +16,47 @@ typedef struct{
     int16_t z;
 } vector3_t;
 
+//*****************************************************************************
+// The interrupt handler for the for SysTick interrupt.
+// Makes a call to getAcclData() to get the acceleration data at the time of the
+// sysTick interrupt. It then writes the acceleration data to the circular 
+// buffer.
+//*****************************************************************************
 void SysTickIntHandler(void);
 
+/*********************************************************
+ * initialises the acceleration
+ *********************************************************/
 void initAccl (void);
 
+/********************************************************
+ * Function to read acceleration from the accelerometer
+ * and return the acceleration
+ ********************************************************/
 vector3_t getAcclData (void);
 
+/********************************************************
+ * Function to convert and return the accelartion from raw to G's
+ ********************************************************/
 vector3_t convertAcceleration (vector3_t acceleration_raw);
 
+/********************************************************
+ * Function to calulate and return the mean acceleration value 
+ * meanVec
+ ********************************************************/
 vector3_t getMeanAccel();
 
+/********************************************************
+ * Function to calculate the acceleration - the offset acceleration
+ * based of the mean acceleration. The acceleration is then converted
+ * to G's and returned.
+ ********************************************************/
 vector3_t calculateAcceleration(vector3_t offSet);
 
+/********************************************************
+ * Initialises the circular buffers with the given buffer
+ * size: BUF_SIZE
+ ********************************************************/
 void initialiseBuffers(void);
 
 
